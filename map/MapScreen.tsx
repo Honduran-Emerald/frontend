1b0a5ccca41cb11fbe8b5cfca8a7d21c53ed4bb7
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-05-06 23:03:34 +0200

    :construction: add userlocation to map

--- map/MapScreen.tsx
+++ map/MapScreen.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import MapView from 'react-native-maps'
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export const MapScreen = () => {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
   return(
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {(location != null && location.coords != null) ?
+      <MapView
+        zoomEnabled={false}
+        scrollEnabled={true}
+        showsUserLocation={true}
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        showsPointsOfInterest={false}
+        // @ts-ignore
+        region={{...location.coords, latitudeDelta: 0.0461, longitudeDelta: 0.0210}}
+      />
+      : <MapView style={styles.map} />}
     </View>
   );
 };
