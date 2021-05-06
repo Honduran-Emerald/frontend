bd44adf6c67c1bbe5841546611f7e8478c8605a7
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-05-06 20:21:34 +0200

    :sparkles: add MapNavigator component

---
+++ map/MapNavigator.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export const MapNavigator = () => {
+    return (
+        <Stack.Navigator>
+
+        </Stack.Navigator>
+    );
+};
