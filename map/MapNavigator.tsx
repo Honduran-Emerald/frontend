95822c37d70fb9aec1622a03252106b6a29d82ad
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-05-06 20:48:27 +0200

    Add MapScreen component and remove headers
    :sparkles: Add MapScreen component to display MapView
    :lipstick: Remove Header from every Screen in MapNavigator

--- map/MapNavigator.tsx
+++ map/MapNavigator.tsx
@@ -1,13 +1,17 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { MapScreen } from './MapScreen';
 
 const Stack = createStackNavigator();
 
 export const MapNavigator = () => {
-    return (
-        <Stack.Navigator>
-
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      screenOptions={(route) => ({
+        headerShown: false
+      })}
+    >
+      <Stack.Screen name='MapScreen' component={MapScreen}/>
+    </Stack.Navigator>
+  );
 };
