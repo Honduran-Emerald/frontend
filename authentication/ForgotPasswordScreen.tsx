c259673558bb08f7d3a0268d9f319abe99ce841a
Felix Wagner <felixwagner99@googlemail.com>
2021-05-09 22:11:04 +0200

    🚧 fetch missing, waiting for api interface

--- authentication/ForgotPasswordScreen.tsx
+++ authentication/ForgotPasswordScreen.tsx
@@ -2,14 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+const emailRegex = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
+
 export default function ForgotPasswordScreen({ navigation }: any) {
 
   const [email, setEmail] = React.useState('');
   const [error, setError] = React.useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
 
   const handleEmailSubmit = () => {
     //TODO fetch to get password reset email
+    setIsButtonDisabled(true);
     setError(true);
+    setIsButtonDisabled(false);
   };
 
   return (
@@ -37,10 +42,10 @@ export default function ForgotPasswordScreen({ navigation }: any) {
       </View>
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button color={'#1D79AC'} title={'Send reset email'} onPress={handleEmailSubmit}/>
+          <Button color={'#1D79AC'} disabled={isButtonDisabled} title={'Send reset email'} onPress={handleEmailSubmit}/>
         </View>
         <View style={styles.button}>
-          <Button color={'#41A8DF'} title={'Back'} onPress={() => {navigation.navigate('Login')}}/>
+          <Button color={'#41A8DF'} disabled={isButtonDisabled} title={'Back'} onPress={() => {navigation.navigate('Login')}}/>
         </View>
       </View>
       <StatusBar style={'auto'}/>
