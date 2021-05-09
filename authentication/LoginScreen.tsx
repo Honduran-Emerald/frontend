852b1148dfc2a1cf7137ffa8ff6b0c6613ec0b8e
Felix Wagner <felixwagner99@googlemail.com>
2021-05-10 00:24:03 +0200

    🚧 cleanup and updates to authentication screens
    💄 added detailed error feedback (needs translation)
    ⚡️ added input checks before performing api calls
    🚧 fetches need to be adjusted to json format, not working right now

--- authentication/LoginScreen.tsx
+++ authentication/LoginScreen.tsx
@@ -1,16 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as SecureStore from "expo-secure-store";
+
+export const backendIP = 'https://emerald.astrogd.cloud'
+
+const emailRegex = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
+
+async function save(key: string, value: string) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+//TODO move to loading screen class to check cached token
+async function getValueFor(key: string) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    return result;
+  } else {
+    return null;
+  }
+}
 
 export default function LoginScreen({ navigation }: any) {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState(false);
+  const [errorEmail, setErrorEmail] = React.useState(false);
+  const [errorPassword, setErrorPassword] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('Email or password incorrect');
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
+  //TODO remove token hook, add prop again, remove rendering of token, how to retrieve token from response
+  const [token, setToken] = React.useState('No token');
+
 
   const handleLogin = () => {
-    //TODO
-    setError(true);
+
+    setIsButtonDisabled(true);
+
+    setError(false); setErrorEmail(false); setErrorPassword(false);
+
+    if(email.length === 0 || !emailRegex.test(email)) {
+      setErrorMessage('Please enter a valid email');
+      setError(true);
+      setErrorEmail(true);
+      setIsButtonDisabled(false);
+      return;
+    } else if(password.length === 0) {
+      setErrorMessage('Please enter a password');
+      setError(true);
+      setErrorPassword(true);
+      setIsButtonDisabled(false);
+      return;
+    } else {
+      setErrorMessage('Email or password incorrect');
+    }
+
+    let formData = new FormData();
+    formData.append('Email', email);
+    formData.append('Password', password);
+
+    const requestOptions = {
+      method: 'POST',
+      body: formData,
+    };
+
+    fetch(`${backendIP}/auth/login`, requestOptions)
+      .then((res) =>
+        res.json().then(data => {
+          if (res.status !== 200) {
+            setToken(`${res.status}`);
+            setError(true);
+            setErrorEmail(true);
+            setErrorPassword(true);
+          } else {
+            save('UserToken', data);
+            setToken(data);
+          }
+          setIsButtonDisabled(false);
+        })
+      )
   };
 
   const handleRegister = () => {
@@ -27,15 +96,16 @@ export default function LoginScreen({ navigation }: any) {
     <View style={styles.container}>
       <Text style={styles.header}>HONDURAN EMERALD</Text>
       <TextInput
-        style={{...styles.input, borderColor: error ? '#d32f2f' : '#111111'}}
+        style={{...styles.input, borderColor: errorEmail ? '#d32f2f' : '#111111'}}
         onChangeText={setEmail}
         value={email}
         placeholder={'Email'}
+        keyboardType={'email-address'}
         returnKeyType={'next'}
         autoCorrect={false}
       />
       <TextInput
-        style={{...styles.input, borderColor: error ? '#d32f2f' : '#111111'}}
+        style={{...styles.input, borderColor: errorPassword ? '#d32f2f' : '#111111'}}
         onChangeText={setPassword}
         value={password}
         placeholder={'Password'}
@@ -50,21 +120,22 @@ export default function LoginScreen({ navigation }: any) {
       <View style={styles.spacer}>
         {
           error &&
-          <Text style={styles.errorText}>Email or password incorrect</Text>
+          <Text style={styles.errorText}>{errorMessage}</Text>
         }
         {
           !error &&
-          <Text style={styles.hiddenText}>Email or password incorrect</Text>
+          <Text style={styles.hiddenText}>{errorMessage}</Text>
         }
       </View>
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button color={'#1D79AC'} title={'Login'} onPress={handleLogin}/>
+          <Button color={'#1D79AC'} disabled={isButtonDisabled} title={'Login'} onPress={handleLogin}/>
         </View>
         <View style={styles.button}>
-          <Button color={'#41A8DF'} title={'Create new account'} onPress={handleRegister}/>
+          <Button color={'#41A8DF'} disabled={isButtonDisabled} title={'Create new account'} onPress={handleRegister}/>
         </View>
       </View>
+      <Text>{token}</Text>
       <StatusBar style={'auto'}/>
     </View>
   );
--- authentication/RegisterScreen.tsx
+++ authentication/RegisterScreen.tsx
@@ -2,18 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+import {backendIP} from './LoginScreen';
+
+const emailRegex = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
 
 async function save(key: string, value: string) {
   await SecureStore.setItemAsync(key, value);
-}
-
-async function getValueFor(key: string) {
-  let result = await SecureStore.getItemAsync(key);
-  if (result) {
-    return result;
-  } else {
-    return null;
-  }
 }
 
 export default function RegisterScreen({ navigation }: any) {
@@ -22,41 +16,73 @@ export default function RegisterScreen({ navigation }: any) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState(false);
+  const [errorName, setErrorName] = React.useState(false);
+  const [errorEmail, setErrorEmail] = React.useState(false);
+  const [errorPassword, setErrorPassword] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('Username or email already used');
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
+  //TODO remove token hook, add prop again, remove rendering of token, how to retrieve token from response
+  const [token, setToken] = React.useState('No token');
 
   const handleRegister = () => {
-    //TODO
+
+    setIsButtonDisabled(true);
+
+    setError(false); setErrorName(false); setErrorEmail(false); setErrorPassword(false);
+
+    if(username.length === 0) {
+      setErrorMessage('Please enter a username');
+      setError(true);
+      setErrorName(true);
+      setIsButtonDisabled(false);
+      return;
+    } else if(email.length === 0 || !emailRegex.test(email)) {
+      setErrorMessage('Please enter a valid email');
+      setError(true);
+      setErrorEmail(true);
+      setIsButtonDisabled(false);
+      return;
+    } else if(password.length === 0) {
+      setErrorMessage('Please enter a password');
+      setError(true);
+      setErrorPassword(true);
+      setIsButtonDisabled(false);
+      return;
+    } else {
+      setErrorMessage('Username or email already used');
+    }
+
+    let formData = new FormData();
+    formData.append('Username', username);
+    formData.append('Email', email);
+    formData.append('Password', password);
+
     const requestOptions = {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: username,
-        email: email,
-        password: password,
-      })
+      body: formData,
     };
 
-    fetch(``, requestOptions)
-      .then((res) => {
-        res.json().then((data) => {
-          if(data.errors) {
-            setError(true)
+    fetch(`${backendIP}/auth/create`, requestOptions)
+      .then((res) =>
+        res.json().then(data => {
+          if (res.status !== 200) {
+            setError(true);
+            data.Code === 'DuplicateUserName' ? setErrorName(true) : setErrorEmail(true);
+            setErrorMessage(data.Description);
           } else {
-            setUsername(username)
-            save('UserToken', data.token);
+            save('UserToken', data);
+            setToken(data);
           }
+          setIsButtonDisabled(false);
         })
-      })
-
-    setError(true);
+      )
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>HONDURAN EMERALD</Text>
       <TextInput
-        style={{...styles.input, borderColor: error ? '#d32f2f' : '#111111'}}
+        style={{...styles.input, borderColor: errorName ? '#d32f2f' : '#111111'}}
         onChangeText={setUsername}
         value={username}
         placeholder={'Username'}
@@ -64,15 +90,16 @@ export default function RegisterScreen({ navigation }: any) {
         autoCorrect={false}
       />
       <TextInput
-        style={{...styles.input, borderColor: error ? '#d32f2f' : '#111111'}}
+        style={{...styles.input, borderColor: errorEmail ? '#d32f2f' : '#111111'}}
         onChangeText={setEmail}
         value={email}
         placeholder={'Email'}
+        keyboardType={'email-address'}
         returnKeyType={'next'}
         autoCorrect={false}
       />
       <TextInput
-        style={{...styles.input, borderColor: error ? '#d32f2f' : '#111111'}}
+        style={{...styles.input, borderColor: errorPassword ? '#d32f2f' : '#111111'}}
         onChangeText={setPassword}
         value={password}
         placeholder={'Password'}
@@ -84,21 +111,22 @@ export default function RegisterScreen({ navigation }: any) {
       <View style={styles.spacer}>
         {
           error &&
-          <Text style={styles.errorText}>Username or email already used</Text>
+          <Text style={styles.errorText}>{errorMessage}</Text>
         }
         {
           !error &&
-          <Text style={styles.hiddenText}>Username or email already used</Text>
+          <Text style={styles.hiddenText}>{errorMessage}</Text>
         }
       </View>
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button color={'#1D79AC'} title={'Create account'} onPress={handleRegister}/>
+          <Button color={'#1D79AC'} disabled={isButtonDisabled} title={'Create account'} onPress={handleRegister}/>
         </View>
         <View style={styles.button}>
-          <Button color={'#41A8DF'} title={'Back'} onPress={() => {navigation.navigate('Login')}}/>
+          <Button color={'#41A8DF'} disabled={isButtonDisabled} title={'Back'} onPress={() => {navigation.navigate('Login')}}/>
         </View>
       </View>
+      <Text>{token}</Text>
       <StatusBar style={'auto'}/>
     </View>
   );
