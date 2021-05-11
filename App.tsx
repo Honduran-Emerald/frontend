225f8788ff609496ed7da60e0ade6258dd311249
Felix Wagner <felixwagner99@googlemail.com>
2021-05-12 01:32:17 +0200

    🚚 moved navbar to new file

--- App.tsx
+++ App.tsx
@@ -1,75 +1,35 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MaterialCommunityIcons }from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import * as SecureStore from 'expo-secure-store';
 
-const Tab = createBottomTabNavigator();
+import AuthNavigator from './authentication/AuthNavigator';
+import MainAppNavigator from './MainAppNavigator';
+
+const Stack = createStackNavigator();
+
+//TODO check for saved token
+async function getToken() {
+  let result = await SecureStore.getItemAsync('UserToken');
+  if (result) {
+    return result;
+  } else {
+    return null;
+  }
+}
 
 export default function App() {
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName : React.ComponentProps<typeof MaterialCommunityIcons>['name'] = "turtle";
-
-            switch (route.name) {
-              case "Home":
-                iconName = "home";
-                break;
-              case "Questlog":
-                iconName = "book-open-variant";
-                break;
-              case "Map":
-                iconName = "map-marker-outline";
-                return (
-                  <View style={{backgroundColor: focused ? "#1D79AC" : "#41A8DF", borderRadius: 100, bottom: 10, padding: 5}}>
-                    <MaterialCommunityIcons name={iconName} size={size + 20} color={"white"}/>
-                  </View>
-                  );
-              case "Chat":
-                iconName = "message-text-outline";
-                break;
-              case "Profile":
-                iconName = "account-outline";
-                break;
-            }
-
-            return <MaterialCommunityIcons name={iconName} size={size + 5} color={color} />;
-          }
-        })}
-        tabBarOptions={{
-          showLabel: false,
-          activeTintColor: "#1D79AC"
-        }}
-      >
-        <Tab.Screen name="Home" component={Dummy}/> 
-        <Tab.Screen name="Questlog" component={Dummy} />
-        <Tab.Screen name="Map" component={Dummy}/>
-        <Tab.Screen name="Chat" component={Dummy}/>
-        <Tab.Screen name="Profile" component={Dummy} />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={() => ({headerShown: false})}>
+        <Stack.Screen name='Authentication' component={AuthNavigator}/>
+        <Stack.Screen name='MainApp' component={MainAppNavigator}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const Dummy = () => {
-  return(
-    <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ MainAppNavigator.tsx
@@ -0,0 +1,71 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons }from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainAppNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName : React.ComponentProps<typeof MaterialCommunityIcons>['name'] = "turtle";
+
+          switch (route.name) {
+            case "Home":
+              iconName = "home";
+              break;
+            case "Questlog":
+              iconName = "book-open-variant";
+              break;
+            case "Map":
+              iconName = "map-marker-outline";
+              return (
+                <View style={{backgroundColor: focused ? "#1D79AC" : "#41A8DF", borderRadius: 100, bottom: 10, padding: 5}}>
+                  <MaterialCommunityIcons name={iconName} size={size + 20} color={"white"}/>
+                </View>
+              );
+            case "Chat":
+              iconName = "message-text-outline";
+              break;
+            case "Profile":
+              iconName = "account-outline";
+              break;
+          }
+
+          return <MaterialCommunityIcons name={iconName} size={size + 5} color={color} />;
+        }
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: "#1D79AC"
+      }}
+    >
+      <Tab.Screen name="Home" component={Dummy}/>
+      <Tab.Screen name="Questlog" component={Dummy} />
+      <Tab.Screen name="Map" component={Dummy}/>
+      <Tab.Screen name="Chat" component={Dummy}/>
+      <Tab.Screen name="Profile" component={Dummy} />
+    </Tab.Navigator>
+  );
+}
+
+const Dummy = () => {
+  return(
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
