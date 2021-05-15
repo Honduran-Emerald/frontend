6710a06918bdde06bf8342df09e2e556e4779aff
Felix Wagner <felixwagner99@googlemail.com>
2021-05-15 17:08:39 +0200

    ✨ add first loading screen
    no dependency used for now
    using conditional rendering

--- App.tsx
+++ App.tsx
@@ -3,22 +3,13 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import * as SecureStore from 'expo-secure-store';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 import AuthNavigator from './authentication/AuthNavigator';
 import MainAppNavigator from './MainAppNavigator';
 import { TokenContext } from './context/TokenContext';
 
 const Stack = createStackNavigator();
-
-//TODO check for saved token
-async function getToken() {
-  let result = await SecureStore.getItemAsync('UserToken');
-  if (result) {
-    return result;
-  } else {
-    return null;
-  }
-}
 
 export default function App() {
 
@@ -31,6 +22,7 @@ export default function App() {
     <TokenContext.Provider value={{token, updateToken}}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={() => ({headerShown: false})}>
+          <Stack.Screen name='Loading' component={LoadingScreen}/>
           <Stack.Screen name='Authentication' component={AuthNavigator}/>
           <Stack.Screen name='MainApp' component={MainAppNavigator}/>
         </Stack.Navigator>
@@ -39,5 +31,47 @@ export default function App() {
   );
 }
 
+async function getToken() {
+  let result = await SecureStore.getItemAsync('UserToken');
+  if (result) {
+    return result;
+  } else {
+    return null;
+  }
+}
 
+const LoadingScreen = ({ navigation }: any) => {
 
+  const {token , updateToken} = React.useContext(TokenContext);
+
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    //TODO validate token
+    getToken().then((token => {token ? updateToken(token) : null; setIsLoaded(true)}), (() => {setIsLoaded(true)}));
+  });
+
+  React.useEffect(() => {
+    if (isLoaded) {
+      token === '' ? navigation.navigate('Authentication') : navigation.navigate('MainApp')
+    }
+  }, [isLoaded]);
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size='large' color='#1D79AC' />
+      <Text>
+        Getting things ready
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
