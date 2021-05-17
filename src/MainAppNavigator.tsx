908573c63015abd889a622cf54517600b720d454
Inluminous <lenmisch@gmail.com>
2021-05-17 16:32:44 +0200

    :twisted_rightwards_arrows: rebase commits

--- src/MainAppNavigator.tsx
+++ src/MainAppNavigator.tsx
@@ -16,6 +16,7 @@ import QuestlogScreen from './common/QuestlogScreen';
 import { clearQuestState } from './redux/quests/questsSlice';
 import { deleteItemLocally } from './utils/SecureStore';
 import { Colors } from './styles';
+import { ChatNavigator } from './chat/ChatNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -64,7 +65,7 @@ export default function MainAppNavigator() {
       <Tab.Screen name="Home" component={DiscoveryNavigator}/>
       <Tab.Screen name="Questlog" component={QuestlogScreen}/>
       <Tab.Screen name="Map" component={MapNavigator}/>
-      <Tab.Screen name="Chat" component={Dummy}/>
+      <Tab.Screen name="Chat" component={ChatNavigator}/>
       <Tab.Screen name="Profile" component={Dummy} />
     </Tab.Navigator>
   );
