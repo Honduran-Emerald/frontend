4d5043731d80b9fe15916f8d4bd14ab77d19ddf2
Felix Wagner <felixwagner99@googlemail.com>
2021-05-24 18:51:19 +0200

    🩹 add localization to loading screen

--- src/common/LoadingScreen.tsx
+++ src/common/LoadingScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import i18n from 'i18n-js';
+
+import './translations';
 
 export const LoadingScreen = () => {
   return (
     <View style={styles.container}>
         <ActivityIndicator size="large" color="#1D79AC" />
-        <Text>Getting things ready</Text>
+        <Text>{i18n.t('loading')}</Text>
     </View>
   );
 };
