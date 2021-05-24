35cda9ce4f8481edb17a39f094803c0f37099c11
Felix Wagner <felixwagner99@googlemail.com>
2021-05-24 18:51:50 +0200

    🚧 add common/translations

---
+++ src/common/translations.tsx
@@ -0,0 +1,18 @@
+import i18n from 'i18n-js';
+
+i18n.translations = {
+  en: {
+    'loading': 'Getting things ready',
+    'acceptButton': 'Accept quest',
+    'votes': 'UPVOTES',
+    'plays': 'PLAYED',
+    'finished': 'FINISHED',
+  },
+  de: {
+    'loading': 'Bereite App vor',
+    'acceptButton': 'Quest akzeptieren',
+    'votes': 'UPVOTES',
+    'plays': 'GESPIELT',
+    'finished': 'BEENDET',
+  },
+};
