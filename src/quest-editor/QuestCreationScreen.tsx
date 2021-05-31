d77606997fbb7ff9abe18ad774c596b0696389b5
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-05-31 21:53:05 +0200

    :sparkles: add input for setting title of quest

--- src/quest-editor/QuestCreationScreen.tsx
+++ src/quest-editor/QuestCreationScreen.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { Colors } from '../styles';
 
 export const QuestCreationScreen = () => {
-    return(
-        <View>
+  const [questTitle, setQuestTitle] = useState<string>("");
 
-        </View>
-    );
+  return(
+    <ScrollView>
+      <TextInput theme={{colors: {primary: Colors.primary}}} value={questTitle} onChange={(text) => {setQuestTitle(text.nativeEvent.text)}} placeholder='Quest Title'/>
+    </ScrollView>
+  );
 }
