d01889106bd8b7fcb33d4c680f70187e4fbbf280
Felix Wagner <felixwagner99@googlemail.com>
2021-05-31 20:34:31 +0200

    ✨ add clearing of quest state to logout, move QuestEditorNavigator to chat position

--- src/MainAppNavigator.tsx
+++ src/MainAppNavigator.tsx
@@ -4,16 +4,16 @@ import React, { useState } from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import { MaterialCommunityIcons }from '@expo/vector-icons';
 import * as SecureStore from 'expo-secure-store';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import { MapNavigator } from './map/MapNavigator';
-
 import { ModuleGraph } from './quest-editor/ModuleGraph';
 import { QuestEditorNavigator } from './quest-editor/QuestEditorNavigator';
-        
 import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { setToken, unsetToken } from './redux/authentication/authenticationSlice';
 import { getUserSelfRequest, queryQuestsRequest } from './utils/requestHandler';
-import { ScrollView } from 'react-native-gesture-handler';
+import QuestlogScreen from './common/QuestlogScreen';
+import { clearQuestState } from './redux/quests/questsSlice';
 
 const Tab = createBottomTabNavigator();
 
@@ -55,9 +55,9 @@ export default function MainAppNavigator() {
       }}
     >
       <Tab.Screen name="Home" component={Dummy}/>
-      <Tab.Screen name="Questlog" component={QuestEditorNavigator} />
+      <Tab.Screen name="Questlog" component={QuestlogScreen} />
       <Tab.Screen name="Map" component={MapNavigator}/>
-      <Tab.Screen name="Chat" component={Dummy}/>
+      <Tab.Screen name="Chat" component={QuestEditorNavigator}/>
       <Tab.Screen name="Profile" component={Dummy} />
     </Tab.Navigator>
   );
@@ -71,13 +71,14 @@ const Dummy = () => {
 
   const token = useAppSelector((state) => state.authentication.token);
   const dispatch = useAppDispatch();
-  
+
   const [t, st] = useState<any>(undefined);
   const [u, su] = useState<any>(undefined);
 
   const handleLogout = () => {
     deleteToken().then(() => {});
     dispatch(unsetToken())
+    dispatch(clearQuestState())
   }
 
   return(
@@ -87,7 +88,7 @@ const Dummy = () => {
       <Text>{JSON.stringify(t)}</Text>
       <Text> USER </Text>
       <Text>{JSON.stringify(u)}</Text>
-      
+
       <Button color={'#1D79AC'} title={'Logout'} onPress={handleLogout}/>
       <Button color={'#1D79AC'} title={'Qusts'} onPress={() => {
         queryQuestsRequest()
