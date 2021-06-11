b8265c091eb909b0a6dad4dc452cadcfac7e94ac
Felix Wagner <felixwagner99@googlemail.com>
2021-06-11 17:53:36 +0200

    ✨ add user is saved in authentication slice

--- src/redux/authentication/authenticationSlice.ts
+++ src/redux/authentication/authenticationSlice.ts
@@ -1,11 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../../types/general';
 
 interface AuthenticationState {
     token: string | undefined
+    user: User | undefined
 }
 
 const initialState: AuthenticationState = {
-    token: undefined
+    token: undefined,
+    user: undefined
 }
 
 export const authenticationSlice = createSlice({
@@ -15,12 +18,16 @@ export const authenticationSlice = createSlice({
         setToken: (state, action: PayloadAction<string>) => {
             state.token = action.payload
         },
+        setUser: (state, action: PayloadAction<User>) => {
+            state.user = action.payload
+        },
         unsetToken: (state) => {
             state.token = undefined
+            state.user = undefined
         }
     }
 })
 
-export const { setToken, unsetToken } = authenticationSlice.actions
+export const { setToken, setUser, unsetToken } = authenticationSlice.actions
 
 export default authenticationSlice.reducer
