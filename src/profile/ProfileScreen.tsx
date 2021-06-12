91e1505f161b765ae55fc8b598abb221db14b3a4
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-06-13 00:48:07 +0200

    :lipstick: add ScrollView to ProfileScreen, add insets to outmost container

--- src/profile/ProfileScreen.tsx
+++ src/profile/ProfileScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from '../styles';
 
 export const ProfileScreen = () => {
+  const insets = useSafeAreaInsets();
   return(
-    <View>
-      
+    <View style={[style.screen, {marginTop: insets.top, marginBottom: insets.bottom}]}>
+      <ScrollView>
+        
+      </ScrollView>
     </View>
   );
-} 
+}
+
+const style = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  }
+})
