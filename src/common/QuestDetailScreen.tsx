c4611063410a25ce688946ac9c1beb516ad189d2
Felix Wagner <felixwagner99@googlemail.com>
2021-06-13 14:53:35 +0200

    💄 update delete modal layout and buttons

--- src/common/QuestDetailScreen.tsx
+++ src/common/QuestDetailScreen.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Image, ScrollView, Dimensions, TouchableNativeFeedback, StatusBar} from 'react-native';
 import i18n from 'i18n-js';
 import { Entypo } from '@expo/vector-icons';
-import { Avatar, Modal, Portal } from 'react-native-paper';
+import { Avatar, Modal, Portal, Button as PaperButton } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Provider as PaperProvider } from 'react-native-paper';
 
 import { Colors } from '../styles';
 import { commonTranslations } from './translations';
@@ -37,7 +36,7 @@ export default function QuestDetailScreen({ route }: any) {
   const showModal = () => setModalVisible(true);
   const hideModal = () => setModalVisible(false);
 
-  // TODO image fetch needed, translation for creator buttons and modal
+  // TODO image fetch needed
 
   const handleAccept = () => {
     setIsButtonDisabled(true);
@@ -51,124 +50,119 @@ export default function QuestDetailScreen({ route }: any) {
   };
 
   return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <ScrollView style={styles.scrollContainer} contentContainerStyle={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <View style={styles.scrollView}>
-            <View style={styles.spacer}/>
-            <Text style={styles.header}>
-              {quest.title}
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollContainer} contentContainerStyle={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.scrollView}>
+          <View style={styles.spacer}/>
+          <Text style={styles.header}>
+            {quest.title}
+          </Text>
+          <Image style={styles.image} source={{uri: 'https://wildlife.org/wp-content/uploads/2015/08/Honduran-Emerald-Hummingbird-Image-Credit-Dominic-Sherony.jpg'}}/>
+          <View style={styles.info}>
+            <Entypo name='location-pin' size={24} color='black'/>
+            <Text style={styles.location}>
+              {quest.locationName}
             </Text>
-            <Image style={styles.image} source={{uri: 'https://wildlife.org/wp-content/uploads/2015/08/Honduran-Emerald-Hummingbird-Image-Credit-Dominic-Sherony.jpg'}}/>
-            <View style={styles.info}>
-              <Entypo name='location-pin' size={24} color='black'/>
-              <Text style={styles.location}>
-                {quest.locationName}
-              </Text>
-              <Entypo name='stopwatch' size={24} color='black'/>
-              <Text style={styles.time}>
-                {quest.approximateTime}
-              </Text>
-            </View>
-            <Text style={styles.description}>
-              {quest.description}
+            <Entypo name='stopwatch' size={24} color='black'/>
+            <Text style={styles.time}>
+              {quest.approximateTime}
             </Text>
-            {
-              !isAccepted &&
-              <View style={styles.button}>
-                <Button color={Colors.primary} disabled={isButtonDisabled} title={i18n.t('acceptButton')} onPress={handleAccept}/>
-              </View>
-            }
-            {
-              isAccepted &&
-              <View style={styles.acceptedText}>
-                <MaterialCommunityIcons name='check' size={24} color='green'/>
-                <Text style={{color: 'green'}}>{i18n.t('questAccepted')}</Text>
-              </View>
-            }
-            {
-              isQuestCreator &&
-              <View style={styles.creatorButtons}>
-                <View style={styles.creatorButton}>
-                  <Button color={Colors.primary} disabled={isButtonDisabled} title={'Edit Quest'} onPress={() => alert('Go to edit screen')}/>
-                </View>
-                <View style={styles.creatorButton}>
-                  <Button color={Colors.error} disabled={isButtonDisabled} title={'Delete Quest'} onPress={showModal}/>
-                </View>
-              </View>
-            }
-            <View style={styles.divider}/>
-            <View style={styles.stats}>
-              <View style={styles.center}>
-                <Text style={styles.mediumText}>
-                  {quest.votes}
-                </Text>
-                <Text style={styles.smallText}>
-                  {i18n.t('votes')}
-                </Text>
-              </View>
-              <View style={styles.center}>
-                <Text style={styles.mediumText}>
-                  {quest.plays}
-                </Text>
-                <Text style={styles.smallText}>
-                  {i18n.t('plays')}
-                </Text>
-              </View>
-              <View style={styles.center}>
-                <Text style={styles.mediumText}>
-                  {isNaN(finishRate) ? '0' : finishRate.toFixed(0)}%
-                </Text>
-                <Text style={styles.smallText}>
-                  {i18n.t('finished')}
-                </Text>
-              </View>
-            </View>
-            {
-              // TODO implement go to profile navigation
-            }
-            <TouchableNativeFeedback onPress={() => { alert('Go to profile') }}>
-              <View style={styles.authorView}>
-                <Avatar.Image style={styles.authorAvatar} source={{uri: 'https://pbs.twimg.com/profile_images/1214590755706150913/Jm78BGWD_400x400.jpg'}}/>
-                <View>
-                  <Text style={styles.authorName}>
-                    {quest.ownerName}
-                  </Text>
-                  <Text style={styles.smallText}>
-                    {`${creationDate.getDate()}.${creationDate.getMonth() + 1}.${creationDate.getFullYear()}`}
-                  </Text>
-                </View>
-              </View>
-            </TouchableNativeFeedback>
-            <View style={styles.spacer}/>
           </View>
-        </ScrollView>
-        <Portal>
-          <Modal visible={modalVisible} dismissable onDismiss={hideModal} contentContainerStyle={styles.modal}>
-            <Text style={{fontSize: 24, textAlign: 'center', marginBottom: 15}}>
-              Delete Quest?
-            </Text>
-            <Text style={{fontSize: 16}}>
-              Do you really want to permanently delete this quest?
-            </Text>
-            <Text style={{fontSize: 16}}>
-              If you just want to hide the quest, you may set it to private.
-            </Text>
-            <View style={styles.modalButtons}>
-              <View style={styles.modalButton}>
-                <Button color={Colors.primaryLight} title={'Set to private'} onPress={() => alert('Implement set to private')}/>
+          <Text style={styles.description}>
+            {quest.description}
+          </Text>
+          {
+            !isAccepted &&
+            <View style={styles.button}>
+              <Button color={Colors.primary} disabled={isButtonDisabled} title={i18n.t('acceptButton')} onPress={handleAccept}/>
+            </View>
+          }
+          {
+            isAccepted &&
+            <View style={styles.acceptedText}>
+              <MaterialCommunityIcons name='check' size={24} color='green'/>
+              <Text style={{color: 'green'}}>{i18n.t('questAccepted')}</Text>
+            </View>
+          }
+          {
+            isQuestCreator &&
+            <View style={styles.creatorButtons}>
+              <View style={styles.creatorButton}>
+                <Button color={Colors.primary} disabled={isButtonDisabled} title={i18n.t('editButton')} onPress={() => alert('Go to edit screen')}/>
               </View>
-              <View style={styles.modalButton}>
-                <Button color={Colors.primary} title={'Back'} onPress={hideModal}/>
-              </View>
-              <View style={styles.modalButton}>
-                <Button color={Colors.error} title={'Delete Quest'} onPress={() => alert('Implement delete')}/>
+              <View style={styles.creatorButton}>
+                <Button color={Colors.error} disabled={isButtonDisabled} title={i18n.t('deleteButton')} onPress={showModal}/>
               </View>
             </View>
-          </Modal>
-        </Portal>
-      </View>
-    </PaperProvider>
+          }
+          <View style={styles.divider}/>
+          <View style={styles.stats}>
+            <View style={styles.center}>
+              <Text style={styles.mediumText}>
+                {quest.votes}
+              </Text>
+              <Text style={styles.smallText}>
+                {i18n.t('votes')}
+              </Text>
+            </View>
+            <View style={styles.center}>
+              <Text style={styles.mediumText}>
+                {quest.plays}
+              </Text>
+              <Text style={styles.smallText}>
+                {i18n.t('plays')}
+              </Text>
+            </View>
+            <View style={styles.center}>
+              <Text style={styles.mediumText}>
+                {isNaN(finishRate) ? '0' : finishRate.toFixed(0)}%
+              </Text>
+              <Text style={styles.smallText}>
+                {i18n.t('finished')}
+              </Text>
+            </View>
+          </View>
+          {
+            // TODO implement go to profile navigation
+          }
+          <TouchableNativeFeedback onPress={() => { alert('Go to profile') }}>
+            <View style={styles.authorView}>
+              <Avatar.Image style={styles.authorAvatar} source={{uri: 'https://pbs.twimg.com/profile_images/1214590755706150913/Jm78BGWD_400x400.jpg'}}/>
+              <View>
+                <Text style={styles.authorName}>
+                  {quest.ownerName}
+                </Text>
+                <Text style={styles.smallText}>
+                  {`${creationDate.getDate()}.${creationDate.getMonth() + 1}.${creationDate.getFullYear()}`}
+                </Text>
+              </View>
+            </View>
+          </TouchableNativeFeedback>
+          <View style={styles.spacer}/>
+        </View>
+      </ScrollView>
+      <Portal>
+        <Modal visible={modalVisible} dismissable onDismiss={hideModal} contentContainerStyle={styles.modal}>
+          <Text style={styles.modalTitle}>
+            {i18n.t('modalDeleteTitle')}
+          </Text>
+          <Text style={styles.modalText}>
+            {i18n.t('modalDeleteText')}
+          </Text>
+          <View style={styles.modalButtons}>
+            <View style={styles.modalButton}>
+              <PaperButton color={Colors.primaryLight} compact mode={'outlined'} onPress={() => alert('Implement set to private')}>{i18n.t('modalSetPrivateButton')}</PaperButton>
+            </View>
+            <View style={styles.modalButton}>
+              <PaperButton color={Colors.error} compact mode={'outlined'} onPress={() => alert('Implement delete')}>{i18n.t('deleteButton')}</PaperButton>
+            </View>
+          </View>
+          <View style={styles.modalBackButton}>
+            <PaperButton color={Colors.primary} compact onPress={hideModal}>{i18n.t('modalBackButton')}</PaperButton>
+          </View>
+        </Modal>
+      </Portal>
+    </View>
   );
 }
 
@@ -280,14 +274,26 @@ const styles = StyleSheet.create({
     padding: 20,
     margin: 20,
   },
+  modalTitle: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  modalText: {
+    fontSize: 16,
+  },
   modalButtons: {
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginTop: 20,
+    marginBottom: 10,
   },
   modalButton: {
-    width: '30%',
+    width: '48%',
+  },
+  modalBackButton: {
+    alignItems: 'flex-start',
   },
 });
