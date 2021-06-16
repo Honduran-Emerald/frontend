b4704144271cb06e7509e0542d20b74ae44a61ec
Hannah <hannah.klohr@gmail.com>
2021-06-16 17:45:30 +0200

    :sparkles: outhouse location

---
+++ src/redux/location/locationSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {LocationObject} from "expo-location";
+
+interface LocationState {
+    location: LocationObject | undefined
+}
+
+const initialState: LocationState = {
+    location: undefined
+}
+
+export const locationSlice = createSlice({
+    name: 'location',
+    initialState,
+    reducers: {
+        setLocation: (state, action: PayloadAction<LocationObject>) => {
+            state.location = action.payload
+        },
+        unsetLocation: (state) => {
+            state.location = undefined
+        }
+    }
+})
+
+export const { setLocation, unsetLocation } = locationSlice.actions
+
+export default locationSlice.reducer
