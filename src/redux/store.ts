f42237860277449a95d58be0febd0c8d54d91c9e
Inluminous <lenmisch@gmail.com>
2021-06-17 22:44:28 +0200

    :sparkles: Add chatReducer to redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from './authentication/authenticationSlice';
 import questsReducer from './quests/questsSlice';
 import editorReducer from './editor/editorSlice';
+import chatReducer from './chat/chatSlice';
 
 export const store = configureStore({
     reducer: {
         authentication: authenticationReducer,
         quests: questsReducer,
         editor: editorReducer,
+        chat: chatReducer
     },
 })
 
