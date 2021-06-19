84ce39c08e20376d7337aed69483772c67a4ed7a
Inluminous <lenmisch@gmail.com>
2021-06-20 00:05:25 +0200

    :sparkles: add chat slice functions for personal messages

--- src/redux/chat/chatSlice.ts
+++ src/redux/chat/chatSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import _ from 'lodash'
+import { ChatMessage } from '../../types/general';
 
 interface ChatPreview {
   userId: string,
@@ -12,12 +13,14 @@ interface ChatPreview {
 
 interface ChatState {
   FCNtoken: string | undefined
-  chatsList: ChatPreview[] | undefined
+  chatsPreviewList: ChatPreview[] | undefined,
+  loadedChats: [string, ChatMessage[]][]
 }
 
 const initialState: ChatState = {
   FCNtoken: undefined,
-  chatsList: undefined,
+  chatsPreviewList: undefined,
+  loadedChats: []
 }
 
 export const chatSlice = createSlice({
@@ -30,16 +33,25 @@ export const chatSlice = createSlice({
     unsetToken: (state) => {
       state.FCNtoken = undefined
     },
+
     loadChatPreview: (state, action: PayloadAction<ChatPreview[]>) => {
-      state.chatsList = _.sortBy(action.payload, (o => o.newestMessage))  
+      state.chatsPreviewList = action.payload 
     },
     updateChatPreview: (state, action: PayloadAction<ChatPreview>) => {
-      state.chatsList = state.chatsList?.filter(cp => cp.userId !== action.payload.userId)
-      state.chatsList?.unshift(action.payload)
+      state.chatsPreviewList = state.chatsPreviewList?.filter(cp => cp.userId !== action.payload.userId)
+      state.chatsPreviewList?.unshift(action.payload)
+    },
+
+    loadPersonalChat: (state, action: PayloadAction<[string, ChatMessage[]]>) => {
+      state.loadedChats = state.loadedChats.filter(chat => chat[0] !== action.payload[0])
+      state.loadedChats.push([action.payload[0], action.payload[1]])
+    },
+    appendPersonalChat: (state, action: PayloadAction<[string, ChatMessage]>) => {
+      state.loadedChats.find(chat => chat[0] === action.payload[0])?.[1].push(action.payload[1])
     }
   }
 })
 
-export const { setToken, unsetToken, loadChatPreview } = chatSlice.actions
+export const { setToken, unsetToken, loadChatPreview, updateChatPreview, loadPersonalChat, appendPersonalChat } = chatSlice.actions
 
 export default chatSlice.reducer
