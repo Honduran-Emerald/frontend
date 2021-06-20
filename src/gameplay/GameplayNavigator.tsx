4a9b1903de715cbe2e7ddb1feacb7cbd410dd58c
Inluminous <lenmisch@gmail.com>
2021-06-21 00:14:44 +0200

    :truck: move questlog to gameplay

--- src/gameplay/GameplayNavigator.tsx
+++ src/gameplay/GameplayNavigator.tsx
@@ -3,12 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { View } from 'react-native';
 
 import { GameplayScreen } from './GameplayScreen';
+import QuestlogScreen from './QuestlogScreen';
 
 const Stack = createStackNavigator();
 
 export const GameplayNavigator = () => {
   return(
     <Stack.Navigator>
+      <Stack.Screen name='QuestlogScreen' component={QuestlogScreen} options={{headerShown: false}}/>
       <Stack.Screen name='GameplayScreen' component={GameplayScreen}/>
       <Stack.Screen name='QuestSettings' component={Dummy}/>
     </Stack.Navigator>
