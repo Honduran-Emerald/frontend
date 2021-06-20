ff1acbf9757b765b53a9dcfef031d8e46c536990
Inluminous <lenmisch@gmail.com>
2021-06-21 01:43:25 +0200

    :sparkles: add gameplay visual base

---
+++ src/gameplay/styleGameplay.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../styles";
+
+export const styleGameplay = StyleSheet.create({
+  bubble: {
+    minWidth: '80%',
+    backgroundColor: Colors.primary,
+    borderRadius: 10,
+    padding: 20,
+    marginVertical: 10,
+    color: '#fff',
+  }
+})
