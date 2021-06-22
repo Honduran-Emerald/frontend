f1cdd9efa204aa97c49f67d6e3f4922f04ac199f
Felix Wagner <felixwagner99@googlemail.com>
2021-06-22 18:07:16 +0200

    🚧 add base layout

--- src/gameplay/modules/ChoiceModule.tsx
+++ src/gameplay/modules/ChoiceModule.tsx
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { PrototypeChoiceModule } from '../../types/quest';
-import { SingleComponentProps } from '../ComponentRenderer';
-import { ModuleRendererProps } from '../ModuleRenderer';
+import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
 
-export const ChoiceModule: React.FC<ModuleRendererProps<PrototypeChoiceModule>> = () => {
+import { ModuleRendererProps } from '../ModuleRenderer';
+import { styleGameplay } from '../styleGameplay';
+import { Colors } from '../../styles';
+import { PrototypeChoiceModule } from '../../types/quest';
+
+export const ChoiceModule: React.FC<ModuleRendererProps<PrototypeChoiceModule>> = (props) => {
+
+  const [hasChosen, setHasChosen] = React.useState(undefined);
+
+  const handleClick = (choice: any, index: number) => {
+    console.log('Pressed ' + (index));
+    setHasChosen(choice);
+  }
 
   return (
-    <Text>ChoiceModule</Text>
+    <View>
+      <Text style={styleGameplay.bubble}>{props.module.module.objective}</Text>
+      {
+        !hasChosen &&
+        props.module.module.choices.map((choice: any, index: number) =>
+          <TouchableNativeFeedback key={index+1} onPress={() => handleClick(choice, index)}>
+            <View style={styles.choice}>
+              <Text style={styles.text}>{choice.text}</Text>
+            </View>
+          </TouchableNativeFeedback>
+        )
+      }
+      {
+        hasChosen &&
+        <View style={styles.chosen}>
+          {
+            // @ts-ignore
+            <Text style={styles.text}>{hasChosen.text}</Text>
+          }
+        </View>
+      }
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  choice: {
+    backgroundColor: Colors.primaryLight,
+    alignItems: 'center',
+    borderRadius: 50,
+    padding: 10,
+    margin: 5,
+  },
+  chosen: {
+    backgroundColor: Colors.secondary,
+    alignItems: 'center',
+    borderRadius: 50,
+    padding: 10,
+    margin: 5,
+  },
+  text: {
+    color: '#fff',
+  },
+});
