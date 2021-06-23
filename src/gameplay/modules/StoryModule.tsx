600d4501c151ec9587f16a16d4b1466a14b057a7
Felix Wagner <felixwagner99@googlemail.com>
2021-06-23 22:35:58 +0200

    ✨ hide continue button after completing story module

--- src/gameplay/modules/StoryModule.tsx
+++ src/gameplay/modules/StoryModule.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Button, Text } from 'react-native';
+import { StyleSheet, TouchableNativeFeedback, View } from 'react-native';
+import { Text } from 'react-native';
 import { GameplayStoryModule } from '../../types/quest';
 import { ModuleRendererProps } from '../ModuleRenderer';
+import {Colors} from "../../styles";
 
 export const StoryModule: React.FC<ModuleRendererProps<GameplayStoryModule>> = ({ module, onChoice }) => {
 
+  const [hasContinued, setHasContinued] = React.useState(!!module.memento);
+
+  const handleClick = () => {
+    setHasContinued(true);
+    onChoice(0);
+  }
+
   return (
     <View>
-      <Button title={'Story Continue'} onPress={() => {onChoice(0)}}/>
+      {
+        !hasContinued &&
+        <View style={styles.container}>
+          <TouchableNativeFeedback onPress={() => handleClick()}>
+            <View style={styles.choice}>
+              <Text style={styles.text}>Continue Story</Text>
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      }
     </View>
-    
-    /* <Text>StoryModule: {module.module.id}</Text> */
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 5,
+  },
+  choice: {
+    backgroundColor: Colors.primaryLight,
+    alignItems: 'center',
+    borderRadius: 50,
+    padding: 10,
+    margin: 5,
+  },
+  text: {
+    color: '#fff',
+  },
+});
