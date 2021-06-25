0897b972c761dfe8baf180b2c7b87ed8c4d99c5a
Felix Wagner <felixwagner99@googlemail.com>
2021-06-25 14:34:37 +0200

    ✨ add new screens to chat navigator

--- src/chat/ChatNavigator.tsx
+++ src/chat/ChatNavigator.tsx
@@ -4,6 +4,8 @@ import { Image } from 'react-native';
 import { ChatOverview } from './overview/ChatOverview';
 import { ChatHeaderTitle } from './personal/ChatHeaderTitle';
 import { ChatPersonal } from './personal/ChatPersonal';
+import FriendlistScreen from './FriendlistScreen';
+import AddFriendScreen from './AddFriendScreen';
 
 export const ChatNavigator: React.FC = () => {
 
@@ -11,22 +13,24 @@ export const ChatNavigator: React.FC = () => {
 
     return (
         <Stack.Navigator>
-            <Stack.Screen 
-                name='ChatOverview' 
+            <Stack.Screen
+                name='ChatOverview'
                 component={ChatOverview}
                 options={{
                     title: 'Chat',
                     headerTitle: 'Chat'
                 }}/>
-            {/* <Stack.Screen 
-                name='ChatPersonal' 
+            {/* <Stack.Screen
+                name='ChatPersonal'
                 component={ChatPersonal}
-                options={({ route }: {route: any}) => 
+                options={({ route }: {route: any}) =>
                 ({
                     title: route.params?.userName || 'Personal Chat',
                     headerTitle: props => <ChatHeaderTitle {...props} userName={route.params.userName} userImgSource={route.params.userImgSource}/>,
                     headerTitleContainerStyle: {left: 50}
                 })}/> */}
+          <Stack.Screen name={'Friendlist'} component={FriendlistScreen} options={{headerShown: false}}/>
+          <Stack.Screen name={'AddFriend'} component={AddFriendScreen} options={{headerTitle: 'Follow new users'}}/>
         </Stack.Navigator>
     )
 }
