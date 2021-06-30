60ebe4208726f2461f27d5d354327e94665fdf25
Inluminous <lenmisch@gmail.com>
2021-06-30 11:55:55 +0200

    :bug: fix text missing from preview

--- src/quest-editor/module-editor/PreviewModuleScreen.tsx
+++ src/quest-editor/module-editor/PreviewModuleScreen.tsx
@@ -4,8 +4,8 @@ import { Dimensions, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import { ModuleRenderer } from '../../gameplay/ModuleRenderer';
 import { Colors } from '../../styles';
-import { PrototypeModule } from '../../types/prototypes';
-import { GameplayModule } from '../../types/quest';
+import { PrototypeComponent, PrototypeModule } from '../../types/prototypes';
+import { GameplayComponent, GameplayModule } from '../../types/quest';
 
 const displayWidth = Dimensions.get('screen').width
 
@@ -14,16 +14,30 @@ interface IPreviewModuleScreen {
   saveModule: () => void
 }
 
+
+const toGameplayModule = (prototypeModule: PrototypeModule): GameplayModule => 
+  ({...prototypeModule, components: prototypeModule.components.map(toGameplayComponent)})
+
+
+const toGameplayComponent = (prototypeComponent: PrototypeComponent, idx: number): GameplayComponent => {
+  switch (prototypeComponent.type) {
+    case 'Text':
+      return {...prototypeComponent, componentId: idx.toString(), componentType: prototypeComponent.type}
+    case 'Image':
+      return {...prototypeComponent, componentId: idx.toString(), componentType: prototypeComponent.type, imageId: ''} //todo do image id here
+  }
+}
+
 export const PreviewModuleScreen: React.FC<IPreviewModuleScreen> = ({ prototypeModule, saveModule }) => (
+
+
   <View style={{padding: 20, paddingBottom: 40, width: displayWidth}} >
                     
     <ScrollView 
       style={{marginBottom: 20}}>
 
       <View style={{borderWidth: 1, borderRadius: 20, padding: 10, marginVertical: 20, backgroundColor: Colors.background, elevation: 3}}>
-        {/* 
-        //@ts-ignore */}
-        <ModuleRenderer module={{ module: prototypeModule, memento: 0 }} onChoice={(choiceId) => 
+        <ModuleRenderer module={{ module: toGameplayModule(prototypeModule), memento: 0 }} onChoice={(choiceId) => 
           (new Promise((resolve, reject) => {
             setTimeout(() => {
               reject(0);
