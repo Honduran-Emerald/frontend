cc5cec9d7780263dc81d1a93b38b44f98d0cff48
Inluminous <lenmisch@gmail.com>
2021-06-30 10:19:28 +0200

    :sparkles: add view of module in editor preview

--- src/quest-editor/module-editor/PreviewModuleScreen.tsx
+++ src/quest-editor/module-editor/PreviewModuleScreen.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Dimensions, View } from 'react-native';
 import { Button } from 'react-native-paper';
+import { ModuleRenderer } from '../../gameplay/ModuleRenderer';
 import { Colors } from '../../styles';
-import { PrototypeModule } from '../../types/quest';
+import { PrototypeModule } from '../../types/prototypes';
+import { GameplayModule } from '../../types/quest';
 
 const displayWidth = Dimensions.get('screen').width
 
@@ -21,6 +23,15 @@ export const PreviewModuleScreen: React.FC<IPreviewModuleScreen> = ({ prototypeM
       <Text>
         {JSON.stringify(prototypeModule)}
       </Text>
+      <View style={{borderWidth: 1, borderRadius: 20, padding: 10, marginVertical: 10, backgroundColor: Colors.background, elevation: 3}}>
+        <ModuleRenderer module={{ module: prototypeModule, memento: 0 }} onChoice={(choiceId) => 
+          (new Promise((resolve, reject) => {
+            setTimeout(() => {
+              reject(0);
+            }, 1000)
+          }))
+          }/>
+      </View>
 
       <Button 
         mode='contained' 
