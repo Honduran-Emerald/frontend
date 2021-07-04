322efc4831f8163f5377c7d5a97193fcd4f7c6b3
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-07-04 23:16:06 +0200

    :sparkles: add ProfileScreen being able to show correct data of other users' profiles

--- src/profile/ProfileScreen.tsx
+++ src/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {StatusBar as StatusBar2, StyleSheet, TouchableOpacity, View} from 'react-native';
+import { StatusBar as StatusBar2, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ProfileTop } from './ProfileTop';
@@ -7,16 +7,13 @@ import { Colors } from '../styles';
 import { ScrollMenu } from "../discovery/ScrollMenu";
 import { StatusBar } from "expo-status-bar";
 import { getLocation } from "../utils/locationHandler";
-import {
-  createQueryRequest,
-  getUserFollowers,
-  queryQuestsRequest,
-  queryvotedQuestsRequest
-} from "../utils/requestHandler";
-import { GameplayQuestHeader, QuestHeader } from "../types/quest";
+import { createQueryRequest, getUserFollowers, getUserRequest, queryQuestsRequest, queryvotedQuestsRequest } from "../utils/requestHandler";
+import { GameplayQuestHeader } from "../types/quest";
 import { useAppSelector } from '../redux/hooks';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import {useNavigation} from "@react-navigation/core";
+import { useNavigation } from "@react-navigation/core";
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { User } from '../types/general';
 
 export default interface profileProps {
     ownProfile: boolean
@@ -26,20 +23,21 @@ export const ProfileScreen = (props: profileProps) => {
   const insets = useSafeAreaInsets();
 
   const location = useAppSelector(state => state.location.location)
-  const user = useAppSelector((state) => state.authentication.user);
+  const authenticatedUser = useAppSelector((state) => state.authentication.user);
 
+  const [user, setUser] = useState<User>();
   const [quests, setQuests] = useState<GameplayQuestHeader[]>([]);
   const [publishedQuests, setPublishedQuests] = useState<GameplayQuestHeader[]>([]);
   const [completedQuests, setCompletedQuests] = useState<GameplayQuestHeader[]>([]);
   const [draftQuests, setDraftQuests] = useState<GameplayQuestHeader[]>([]);
   const [upvotedQuests, setUpvotedQuests] = useState<GameplayQuestHeader[]>([]);
-  const [followerCount, setFollowerCount] = useState(0);
 
   const navigation = useNavigation();
+  const route = useRoute<RouteProp<{Profile: {userId: string}}, 'Profile'>>();
 
   const lodash = require("lodash")
 
-  useEffect(() => {
+  /*useEffect(() => {
     getUserFollowers().then(res => res.json()).then((followers) => setFollowerCount(followers.users.length));
     queryQuestsRequest().then(res => res.json()).then((quests) => setQuests(quests.quests));
     createQueryRequest(0).then(res => res.json()).then((quests) => {
@@ -63,7 +61,20 @@ export const ProfileScreen = (props: profileProps) => {
     queryvotedQuestsRequest("Up").then(res => res.json()).then((quests) => setUpvotedQuests(quests.quests));
     // Get Location Permission and set initial Location
     getLocation().catch((err: Error) => {});
-  },[])
+  },[])*/
+
+  useEffect(() => {
+    if(route.params?.userId && route.params.userId !== authenticatedUser?.userId) {
+      console.log(route.params.userId);
+      getUserRequest(route.params.userId).then(response => response.json()).then(obj => setUser(obj.user));
+    } else {
+      setUser(authenticatedUser);
+    }
+    
+    queryQuestsRequest(0, route.params?.userId || authenticatedUser?.userId)
+      .then(response => response.json())
+      .then(obj => setPublishedQuests(obj.quests));
+  }, [])
 
   return(
     <View style={[style.screen, {marginTop: insets.top, marginBottom: insets.bottom}]}>
@@ -71,7 +82,14 @@ export const ProfileScreen = (props: profileProps) => {
         <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{top: 5, right: 5, position: "absolute"}}>
             <MaterialCommunityIcons name="cog" size={30} color='#1D79AC' />
         </TouchableOpacity>
-        {user && <ProfileTop ownProfile profileData={{username: user?.userName, followers: followerCount, level: user?.level, xp: user?.experience, profileImageId: user?.image, questsCreated: publishedQuests.length, questsPlayed: 300}} />}
+        {user && authenticatedUser && 
+          <ProfileTop 
+            ownProfile={authenticatedUser.userId === user.userId} 
+            profileData={{username: user.userName, followers: user.followerCount, level: user.level, xp: user.experience, profileImageId: user.image, questsCreated: user.questCount, questsPlayed: user.trackerCount}} 
+            following={user.following}
+            friends={user.follower && user.following}
+          />
+        }
           {location && (
             <>
               <ScrollMenu header={"Published Quests"} type={"published"} location={location} quests={publishedQuests}/>
