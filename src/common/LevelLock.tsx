cd6cfbe968feb60956ed2b3d30eab2e431df986d
Inluminous <lenmisch@gmail.com>
2021-07-11 00:30:51 +0200

    :sparkles: add explanations in level lock

--- src/common/LevelLock.tsx
+++ src/common/LevelLock.tsx
@@ -23,6 +23,15 @@ interface LevelLockProps {
   }
 }
 
+const questLevelLock = /* {
+  questToLevel: (questCount: number) => questCount * (questCount - 1) / 2 + 3,
+  levelToQuest: (level: number) => Math.floor((1 + Math.sqrt(8*level + 1)) / 2)
+} // This one grows quadratically such that the distance between each quest cap grows by 1 */
+{
+  questToLevel: (questCount: number) => questCount * 2 - 3,
+  levelToQuest: (level: number) => Math.floor((3 + level) / 2)
+}
+
 export const LevelLock: React.FC<LevelLockProps> = ({ children, permission, alternative, dialog }) => {
 
   const user = useAppSelector(state => state.authentication.user)
@@ -30,7 +39,7 @@ export const LevelLock: React.FC<LevelLockProps> = ({ children, permission, alte
 
   if (user && !unlockAll &&
       (permission.type === 'discrete' ? permission.perm in levelLocks && user.level < levelLocks[permission.perm]
-       : permission.type === 'quests' ? permission.quests!==undefined && permission.quests >= Math.sqrt(user.level) + 1 : false)) {
+       : permission.type === 'quests' ? permission.quests !== undefined && permission.quests + 1 > questLevelLock.levelToQuest(user.level) : false)) {
         
     return <>
       {dialog && 
@@ -56,8 +65,12 @@ export const LevelLock: React.FC<LevelLockProps> = ({ children, permission, alte
           height: '100%', 
           textAlignVertical: 'center',
           fontSize: 20,
-          color: Colors.error,
-          }} onPress={() => {setShowNoMoreQuestsDialog(true)}}>Level too low</Subheading>
+          color: Colors.primaryLight,
+          }} onPress={() => {setShowNoMoreQuestsDialog(true)}}>
+            {permission.type === 'discrete' && permission.perm in levelLocks && levelLocks[permission.perm] < 1/0 ? `Reach level ${levelLocks[permission.perm]} to unlock` 
+            : permission.type === 'quests' && permission.quests !== undefined ? `Reach level ${ questLevelLock.questToLevel(permission.quests + 1) } to create more quests`
+            : 'Level too low'}
+            </Subheading>
       </TouchableWithoutFeedback>} 
     </>
     
